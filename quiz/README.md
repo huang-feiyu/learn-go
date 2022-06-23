354b99a6e3b27257db2c9a706f4b5b78899ee404
huang-feiyu <365429526@qq.com>
2022-06-23 14:41:08 +0800

    ex1: finish part1

---
+++ quiz/README.md
@@ -0,0 +1,16 @@
+# quiz
+
+> [gophercises](https://github.com/gophercises/quiz)
+
+[TOC]
+
+## Part I
+
+```bash
+> ./quiz -h
+Usage of ./quiz:
+  -csv string
+      a csv file in the format of "question,answer" (default "problems.csv")
+```
+
+It is very easy to implement.
--- quiz/main.go
+++ quiz/main.go
@@ -1,9 +1,56 @@
 package main
 
 import (
+	"encoding/csv"
+	"flag"
 	"fmt"
+	"log"
+	"os"
+	"strings"
 )
 
+func readCsvFile(fileName string) [][]string {
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal("Unable to read input file "+fileName, err)
+	}
+	// ensure to close the file
+	defer file.Close()
+
+	csvReader := csv.NewReader(file)
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal("Unable to parse file as CSV for "+fileName, err)
+	}
+
+	// csv line: question,answer
+	return records
+}
+
+func startQuiz(expressions [][]string) (corrcet, all int) {
+	all = len(expressions)
+	corrcet = 0
+
+	for i, expression := range expressions {
+		fmt.Printf("Problem #%d: %s = ", i+1, expression[0])
+		var answer string
+		if _, err := fmt.Scan(&answer); err != nil {
+			log.Fatal("Unable to read answer", err)
+		}
+		// remove spaces from answer
+		if strings.ReplaceAll(answer, " ", "") == expression[1] {
+			corrcet++
+		}
+	}
+
+	return
+}
+
 func main() {
-	fmt.Println("hello world")
+	file := flag.String("csv", "problems.csv", "a csv file in the format of \"question,answer\"")
+	flag.Parse()
+	expressions := readCsvFile(*file)
+
+	correct, all := startQuiz(expressions)
+	fmt.Println("You scored", correct, "out of", all, "correct")
 }
