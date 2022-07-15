9e51bf635fa1e875b1b0e06ccd02eaba6028094d
huang-feiyu <365429526@qq.com>
2022-07-15 23:21:10 +0800

    intro2ms: finish ep.02

---
+++ product-api/go.mod
@@ -0,0 +1,3 @@
+module huangblog.com/product-api
+
+go 1.18
--- product-api/main.go
+++ product-api/main.go
@@ -1,27 +1,50 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"huangblog.com/product-api/handlers"
 )
 
 func main() {
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		log.Println("Hello, world!")
-		d, err := ioutil.ReadAll(request.Body)
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+	gb := handlers.NewGoodbye(l)
+
+	// sm mux. it is created by default, but we can create our own.
+	sm := http.NewServeMux()
+	// register the handler
+	sm.Handle("/", hh)
+	sm.Handle("/bye", gb)
+
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
 		if err != nil {
-			http.Error(writer, "Oops, something went wrong!", http.StatusBadRequest)
-			return
+			l.Fatal(err)
 		}
+	}()
 
-		fmt.Fprintf(writer, "Hello, %s!", d)
-	})
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
 
-	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
-		log.Println("Goodbye, world!")
-	})
+	sig := <-sigChan
+	l.Println("Received signal:", sig, "Shutting down...")
 
-	http.ListenAndServe(":9090", nil)
+	// everything is done, so we can exit
+	ct, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(ct)
 }
