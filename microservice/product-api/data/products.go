d9d22b8a5d39435140836ded549b13e43b454ee4
Huang <0711feiyu@gmail.com>
2022-07-19 21:36:51 +0800

    microservice: finish ep.04

--- microservice/product-api/data/products.go
+++ microservice/product-api/data/products.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
@@ -16,6 +17,11 @@ type Product struct {
 	CreatedOn   string  `json:"-"`
 	UpdatedOn   string  `json:"-"`
 	DeletedOn   string  `json:"-"`
+}
+
+func (p *Product) FromJSON(r io.Reader) error {
+	e := json.NewDecoder(r)
+	return e.Decode(p)
 }
 
 // Products is a collection of Product
@@ -35,6 +41,40 @@ func (p *Products) ToJSON(w io.Writer) error {
 // GetProducts returns a list of products
 func GetProducts() Products {
 	return productList
+}
+
+func AddProduct(prod *Product) {
+	prod.ID = getNextID()
+	prod.CreatedOn = time.Now().UTC().String()
+	productList = append(productList, prod)
+}
+
+func UpdateProduct(id int, prod *Product) error {
+	_, pos, err := findProduct(id)
+	if err != nil {
+		return err
+	}
+
+	prod.ID = id
+	productList[pos] = prod
+
+	return nil
+}
+
+var ErrProductNOTFound = fmt.Errorf("Product not found")
+
+func findProduct(id int) (*Product, int, error) {
+	for i, p := range productList {
+		if p.ID == id {
+			return p, i, nil
+		}
+	}
+	return nil, -1, ErrProductNOTFound
+}
+
+func getNextID() int {
+	lp := productList[len(productList)-1]
+	return lp.ID + 1
 }
 
 // productList is a hard coded list of products for this
--- microservice/product-api/handlers/products.go
+++ microservice/product-api/handlers/products.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 
 	"huangblog.com/product-api/data"
 )
@@ -15,18 +17,94 @@ func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
+/**
+ * https://docs.microsoft.com/zh-cn/azure/architecture/best-practices/api-design
+ **/
 func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		p.GetProducts(rw, r)
-	} else {
+	switch r.Method {
+	case http.MethodGet:
+		p.getProducts(rw, r)
+
+	case http.MethodPost:
+		p.addProducts(rw, r)
+
+	case http.MethodPut:
+		// expect the id in the uri
+		rg := regexp.MustCompile("/([0-9]+)")
+		g := rg.FindAllStringSubmatch(r.URL.Path, -1)
+		if len(g) != 1 {
+			p.l.Println("Invalid URI more than one id")
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+		if len(g[0]) != 2 {
+			p.l.Println("Invalid URI more than one capture group")
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+		idString := g[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			p.l.Println("Invalid URI not a number", idString)
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+		p.updateProducts(id, rw, r)
+
+	default:
 		rw.WriteHeader(http.StatusMethodNotAllowed)
 	}
 }
 
-func (p *Products) GetProducts(rw http.ResponseWriter, r *http.Request) {
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("GET: getProducts")
+
+	// fetch from the database
 	lp := data.GetProducts()
+
+	// serialize the data to list
 	err := lp.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Unable to encode json", http.StatusInternalServerError)
+	}
+}
+
+func (p *Products) addProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("POST: addProducts")
+
+	// get from the request(user)
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to decode json", http.StatusBadRequest)
+		return
+	}
+
+	// add to the database
+	data.AddProduct(prod)
+
+	p.l.Printf("Product: %#v\n", prod)
+}
+
+// put the entire object, expect the id in the URI
+func (p *Products) updateProducts(id int, rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("PUT: updateProducts")
+
+	// get from the request(user)
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to decode json", http.StatusBadRequest)
+		return
+	}
+
+	err = data.UpdateProduct(id, prod)
+	if err == data.ErrProductNOTFound {
+		http.Error(rw, "Product not found", http.StatusNotFound)
+	}
+
+	if err != nil {
+		http.Error(rw, "Something went wrong", http.StatusInternalServerError)
+		return
 	}
 }
